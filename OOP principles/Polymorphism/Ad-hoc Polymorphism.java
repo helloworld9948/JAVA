5f7d3d0a6d2a26ad6f80adb54841f6344c0ca258
helloworld9948 <134037451+helloworld9948@users.noreply.github.com>
2024-03-28 18:10:09 +0530

    Update Ad-hoc Polymorphism.java

--- OOP principles/Polymorphism/Ad-hoc Polymorphism.java
+++ OOP principles/Polymorphism/Ad-hoc Polymorphism.java
@@ -1,23 +1,21 @@
 import java.util.Arrays;
 
-public class AdhocPolymorphismExample {
-
-    public static void sorting(int[] arr) {
-        Arrays.parallelSort(arr);
-        System.out.println("Integers after sort - Array Story: " + Arrays.toString(arr));
+public class AdHocPolymorphismExample {
+    void sorting(int[] list) {
+        Arrays.parallelSort(list);
+        System.out.println("Integers after sort: " + Arrays.toString(list));
     }
 
-    public static void sorting(String[] arr) {
-        Arrays.parallelSort(arr);
-        System.out.println("Names after sort: " + Arrays.toString(arr));
+    void sorting(String[] names) {
+        Arrays.parallelSort(names);
+        System.out.println("Names after sort: " + Arrays.toString(names));
     }
 
     public static void main(String[] args) {
-        int[] numbers = {5, 3, 9, 1, 7};
-        String[] names = {"John", "Alice", "Bob", "David", "Eva"};
-
-        sorting(numbers);
-
-        sorting(names);
+        AdHocPolymorphismExample obj = new AdHocPolymorphismExample();
+        int list[] = {2, 3, 1, 5, 4};
+        obj.sorting(list);  // Calling with integer array
+        String[] names = {"preetham", "charan", "sadvika", "gayatri"};
+        obj.sorting(names); // Calling with String array
     }
 }
