5c98ab31c46f937e6cde65b81a340f77da3c5a17
helloworld9948 <134037451+helloworld9948@users.noreply.github.com>
2024-03-28 18:09:26 +0530

    Update Hierarchical Inheritance.java

--- OOP principles/ Inheritance/Hierarchical Inheritance.java
+++ OOP principles/ Inheritance/Hierarchical Inheritance.java
@@ -1,6 +1,6 @@
 class ParentClass {
     int a;
-    
+
     void setData(int a) {
         this.a = a;
     }
@@ -8,26 +8,28 @@ class ParentClass {
 
 class ChildClass extends ParentClass {
     void showData() {
+        System.out.println("Inside ChildClass!");
         System.out.println("Value of a is " + a);
     }
 }
 
-class Childclassone extends ParentClass {
-    void showData() {
-        System.out.println("Hai");
-    }
-}
-
-class ChildThree extends ChildClass { 
+class ChildClassToo extends ParentClass {
     void display() {
-        System.out.println("Hello");
+        System.out.println("Inside ChildClassToo!");
+        System.out.println("Value of a is " + a);
     }
 }
 
-public class SingleInheritance {
+public class HierarchicalInheritance {
     public static void main(String[] args) {
-        ChildClass obj = new ChildClass();
-        obj.setData(100);
-        obj.showData();
+        // Creating an object of ChildClass
+        ChildClass child_obj = new ChildClass();
+        child_obj.setData(100);
+        child_obj.showData();
+        
+        // Creating an object of ChildClassToo
+        ChildClassToo childToo_obj = new ChildClassToo();
+        childToo_obj.setData(200);
+        childToo_obj.display();
     }
 }
