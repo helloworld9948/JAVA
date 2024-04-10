d888677c9707f348c84efbb4206b274b59627066
helloworld9948 <134037451+helloworld9948@users.noreply.github.com>
2024-04-10 10:43:19 +0530

    Create QueueExample.java

---
+++ Collections/QueueExample.java
@@ -0,0 +1,26 @@
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+public class QueueExample {
+    public static void main(String args[]) {
+        PriorityQueue<String> queue = new PriorityQueue<String>();
+        queue.add("Sree");
+        queue.add("Vij");
+        queue.add("Shiva");
+        queue.add("Hari");
+        System.out.println("head: " + queue.element());
+        System.out.println("head: " + queue.peek());
+        System.out.println("iterating the queue elements:");
+        Iterator<String> itr = queue.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+        queue.remove();
+        queue.poll();
+        System.out.println("after removing two elements:");
+        Iterator<String> itr2 = queue.iterator();
+        while (itr2.hasNext()) {
+            System.out.println(itr2.next());
+        }
+    }
+}
